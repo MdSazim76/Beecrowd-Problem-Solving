7e52d46f6c60847f416141137a9fb21e90c51074
Md_Sazim <93608296+Md-Zihad-Zaman-Sazim@users.noreply.github.com>
2025-03-14 22:30:00 +0600

    Create 1005.cpp

---
+++ 1005.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a,b,sum;
+    cin>>a>>b;
+    sum= (a*3.5+b*7.5)/(3.5+7.5);
+    cout<<fixed<<setprecision(5)<<"MEDIA = "<<sum<<endl;
+    return 0;
+}
