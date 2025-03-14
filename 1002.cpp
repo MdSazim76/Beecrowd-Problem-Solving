1fd0d3974265e3d0b99d3d9d4b411c9d655f5357
Md_Sazim <93608296+Md-Zihad-Zaman-Sazim@users.noreply.github.com>
2025-03-14 21:46:02 +0600

    Create 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double R, n=3.14159, A;
+    cin>>R;
+    A = n*(R*R);
+    cout<<fixed << setprecision(4)<<"A="<<A<<endl;
+    return 0;
+}
