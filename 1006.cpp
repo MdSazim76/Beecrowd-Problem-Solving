64de9d8ffcca6457e8d712e10af3f8b1d7a56087
Md_Sazim <93608296+MdSazim76@users.noreply.github.com>
2025-03-16 01:21:16 +0600

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float A, B, C, Media;
+    cin>>A>>B>>C;
+    Media = (A*2+B*3+C*5)/ (2+3+5);
+    cout<<fixed<<setprecision(1)<<"MEDIA = "<<Media<<endl;
+    return 0;
+}
